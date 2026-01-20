4b82454542c389a9dbf540b6bc17a7b2c5900943
zvx5168 <zvx5168@psu.edu>
2026-01-20 11:33:52 -0500

    created a color

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[]args){
+        Motorcycle m1 = new Motorcycle();
+    }
+
+}
--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -2,5 +2,4 @@ public class Motorcycle {
 
     private String color;
 
-
 }
