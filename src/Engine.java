d4c547000cc0316c77b688b7950a4ff46b01fb4e
zvx5168 <zvx5168@psu.edu>
2026-01-25 19:21:14 -0500

    add motorcycle

---
+++ src/Engine.java
@@ -0,0 +1,13 @@
+public class Engine {
+    private int cc;
+    public Engine(int cc){
+        this.cc =cc;
+    }
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
 public class Main {
     public static void main(String[]args){
-        Motorcycle m1 = new Motorcycle("red");
+        Engine e1 = new Engine(600);
+        Engine e2 = new Engine(800);
+        Engine e3 = new Engine(1000);
+
+
+
+        Motorcycle m1 = new Motorcycle("red","Honda",2020,e1);
         System.out.println(m1.getColor());
+        System.out.println(m1.getBrand());
+        System.out.println(m1.getYear());
+        System.out.println(m1.getEngine().getCc());
 
-        Motorcycle m2 = new Motorcycle("blue");
+        System.out.println("-------");
+
+        Motorcycle m2 = new Motorcycle("blue","Yamaha",2021,e2);
         System.out.println(m2.getColor());
-    }
+        System.out.println(m2.getBrand());
+        System.out.println(m2.getYear());
+        System.out.println(m2.getEngine().getCc());
 
+        System.out.println("-------");
+
+        Motorcycle m3 =new Motorcycle("black","Kawasaki",2022,e3);
+        System.out.println(m3.getColor());
+        System.out.println(m3.getBrand());
+        System.out.println(m3.getYear());
+        System.out.println(m3.getEngine().getCc());
+
+
+    }
 }
