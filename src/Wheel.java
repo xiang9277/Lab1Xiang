fcadb64c24404a978b1a8f52dadf55dcc6d99d6e
zvx5168 <zvx5168@psu.edu>
2026-01-28 22:43:50 -0500

    add Wheel

---
+++ src/Wheel.java
@@ -0,0 +1,9 @@
+public class Wheel {
+    private int size;
+    public  Wheel(int size){
+        this.size = size;
+    }
+    public int getSize(){return size;}
+    public void setSize(int size){this.size = size;}
+
+}
