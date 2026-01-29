d8f4ad9d53d300eaa757e73961b2f102ba9175f2
zvx5168 <zvx5168@psu.edu>
2026-01-28 23:04:47 -0500

    add Motorcycle

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -10,17 +10,22 @@
 public class Motorcycle {
 
     private String color;
-
     private String brand;
     private int year;
     private Engine engine;
+    private Wheel wheel;
+    private Seat seat;
+    private FuelTank tank;
 
-
-    public Motorcycle(String color, String brand, int year, Engine engine){
+    public Motorcycle(String color, String brand, int year, Engine engine,Wheel wheel, Seat seat, FuelTank tank){
         this.color = color;
         this.brand = brand;
         this.year = year;
         this.engine = engine;
+        this.wheel = wheel;
+        this.seat = seat;
+        this.tank = tank;
+
     }
     public String getColor(){
         return this.color;
@@ -42,6 +47,18 @@ public class Motorcycle {
         this.color = color;
     }
 
+    public Wheel getWheel(){
+        return wheel;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public FuelTank getTank() {
+        return tank;
+    }
+
     public void setBrand(String brand) {
         this.brand = brand;
     }
@@ -52,5 +69,17 @@ public class Motorcycle {
 
     public void setEngine(Engine engine) {
         this.engine = engine;
+    }
+
+    public void setWheel(Wheel wheel) {
+        this.wheel = wheel;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public void setTank(FuelTank tank) {
+        this.tank = tank;
     }
 }
