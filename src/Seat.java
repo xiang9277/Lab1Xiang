b34abd11794d45b2c97af65e36be21bd8fce3f2b
zvx5168 <zvx5168@psu.edu>
2026-01-28 22:54:46 -0500

    add Seat

---
+++ src/Seat.java
@@ -0,0 +1,14 @@
+public class Seat {
+    private String material;
+    public Seat(String material){
+        this.material =material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
