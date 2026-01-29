313b5b49756b24bbe14f2b09b3be023fd3d5efb7
zvx5168 <zvx5168@psu.edu>
2026-01-28 22:49:16 -0500

    add FuelTank

---
+++ src/FuelTank.java
@@ -0,0 +1,14 @@
+public class FuelTank {
+    private double capacity;
+    public FuelTank(double capacity){
+        this.capacity =capacity;
+    }
+
+    public double getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(double capacity) {
+        this.capacity = capacity;
+    }
+}
